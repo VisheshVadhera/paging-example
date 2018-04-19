5969977faa0bb3b9e60841a2e539028aa9a30185
Vishesh Vadhera <vishesh.2710@gmail.com>
2018-04-19 19:08:33 +0530

    Cleanup

--- app/src/main/java/com/vishesh/pagingexample/network/RedditActivity.kt
+++ app/src/main/java/com/vishesh/pagingexample/network/RedditActivity.kt
@@ -1,8 +1,5 @@
 package com.vishesh.pagingexample.network
 
-import android.arch.lifecycle.LiveDataReactiveStreams
-import android.arch.lifecycle.Observer
-import android.arch.paging.PagedList
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
@@ -10,11 +7,8 @@ import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import com.bumptech.glide.Glide
 import com.vishesh.pagingexample.R
-import io.reactivex.FlowableSubscriber
 import io.reactivex.Observable
 import kotlinx.android.synthetic.main.activity_reddit.*
-import org.reactivestreams.Subscriber
-import org.reactivestreams.Subscription
 import java.util.concurrent.Executors
 
 class RedditActivity : AppCompatActivity() {
@@ -34,7 +28,6 @@ class RedditActivity : AppCompatActivity() {
         initViewModel()
         initAdapter()
         initRxViewBindings()
-//        viewModel.showSubreddit("androiddev")
     }
 
     private fun initViewModel() {
@@ -51,17 +44,11 @@ class RedditActivity : AppCompatActivity() {
 
     private fun initRxViewBindings() {
         val uiIntent = Observable.just(UiIntent.ShowReddit("androiddev"))
-        /*viewModel.posts.observe(this, Observer {
-            adapter.submitList(it)
-        })*/
-        /*viewModel.statuses.observe(this, Observer {
-            adapter.setNetworkState(it?.networkState)
-        })*/
         uiIntent.compose(viewModel.bind())
                 .subscribe {
                     when (it) {
-                        is UiState.ListReceived -> adapter.submitList(it.list)
-                        is UiState.ResultState -> adapter.setNetworkState(it.result.networkState)
+                        is UiState.List -> adapter.submitList(it.pagedList)
+                        is UiState.State -> adapter.setNetworkState(it.networkState)
                     }
                 }
     }
--- app/src/main/java/com/vishesh/pagingexample/network/RedditViewModel.kt
+++ app/src/main/java/com/vishesh/pagingexample/network/RedditViewModel.kt
@@ -1,15 +1,15 @@
 package com.vishesh.pagingexample.network
 
-import android.arch.lifecycle.LiveData
-import android.arch.lifecycle.MutableLiveData
 import android.arch.paging.DataSource
 import android.arch.paging.ItemKeyedDataSource
 import android.arch.paging.PagedList
 import android.arch.paging.RxPagedListBuilder
+import io.reactivex.BackpressureStrategy
 import io.reactivex.Observable
 import io.reactivex.ObservableTransformer
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
+import io.reactivex.subjects.PublishSubject
 import retrofit2.Call
 import retrofit2.Response
 import java.io.IOException
@@ -17,33 +17,23 @@ import java.util.concurrent.Executor
 
 
 class RedditViewModel(private val repo: RedditPostRepository) {
-    /*private val subredditName = MutableLiveData<String>()
-
-    private val repoResult = map(subredditName, { repo.postsOfSubreddit(it, 30) })
-    val posts = switchMap(repoResult) { it.pagedList }
-    val statuses = switchMap(repoResult) { it.statusUpdates }*/
 
     fun bind(): ObservableTransformer<UiIntent, UiState> {
         return ObservableTransformer {
             val connectableObservable = it
-                                .ofType(UiIntent.ShowReddit::class.java)
-                                .flatMap { repo.postsOfSubreddit(it.subredditName, 30) }
+                    .ofType(UiIntent.ShowReddit::class.java)
+                    .flatMap { repo.postsOfSubreddit(it.subredditName, 30) }
                     .share()
 
-                        val pagedList = connectableObservable
-                                .ofType(PagedList::class.java)
-                                .map { UiState.ListReceived(it as PagedList<RedditPost>) }
+            val pagedList = connectableObservable
+                    .ofType(Result.PagedListResult::class.java)
+                    .map { UiState.List(it.list) }
 
-                        val results = connectableObservable
-                                .ofType(Result::class.java)
-                                .map { UiState.ResultState(it) }
+            val results = connectableObservable
+                    .ofType(Result.NetworkStateResult::class.java)
+                    .map { UiState.State(it.networkState) }
 
-//                        connectableObservable.connect()
-                        return@ObservableTransformer Observable.merge(pagedList, results)
-
-            /*it.ofType(UiIntent.ShowReddit::class.java)
-                    .flatMap { repo.postsOfSubreddit(it.subredditName, 30) }
-                    .map { UiState.ListReceived(it) }*/
+            return@ObservableTransformer Observable.merge(pagedList, results)
         }
     }
 
@@ -57,33 +47,19 @@ class RedditViewModel(private val repo: RedditPostRepository) {
 }
 
 sealed class UiState {
-    class ListReceived(val list: PagedList<RedditPost>) : UiState()
-    class ResultState(val result: Result) : UiState()
+    class List(val pagedList: PagedList<RedditPost>) : UiState()
+    class State(val networkState: NetworkState) : UiState()
 }
 
 interface RedditPostRepository {
-    fun postsOfSubreddit(subReddit: String, pageSize: Int): Observable<PagedList<RedditPost>>
+    fun postsOfSubreddit(subReddit: String, pageSize: Int): Observable<Result>
 }
-
-/*data class Listing<T>(
-        val pagedList: Observable<PagedList<T>>,
-        val statusUpdates: Observable<Result>,
-        val refresh: () -> Unit,
-        val retry: () -> Unit
-)*/
-
-data class Listing<T>(
-        val pagedList: LiveData<PagedList<T>>,
-        val statusUpdates: LiveData<Result>,
-        val refresh: () -> Unit,
-        val retry: () -> Unit
-)
 
 class InMemoryByItemRepo(
         private val redditService: RedditService,
         private val networkExecutor: Executor) : RedditPostRepository {
 
-    override fun postsOfSubreddit(subReddit: String, pageSize: Int): Observable<PagedList<RedditPost>> {
+    override fun postsOfSubreddit(subReddit: String, pageSize: Int): Observable<Result> {
 
         val sourceFactory = SubRedditDataSourceFactory(redditService, subReddit, networkExecutor)
 
@@ -94,66 +70,22 @@ class InMemoryByItemRepo(
                 .build()
         val pagedList = RxPagedListBuilder(sourceFactory, pagedListConfig)
                 .setFetchScheduler(Schedulers.io())
-//                .setNotifyScheduler(AndroidSchedulers.mainThread())
+                .setNotifyScheduler(AndroidSchedulers.mainThread())
                 .buildObservable()
+                .map<Result> { Result.PagedListResult(it) }
 
 
-        /*val statusUpdates = sourceFactory
+        val statusUpdates = sourceFactory
                 .subject
-                .blockingFirst()
-                .pageSubject
+                .flatMap { it.pageSubject }
                 .toFlowable(BackpressureStrategy.LATEST)
-                .toObservable()*/
+                .toObservable()
 
-        return pagedList
+        return Observable.merge(pagedList, statusUpdates)
     }
 }
-
-/*
-class InMemoryByItemRepo(
-        private val redditService: RedditService,
-        private val networkExecutor: Executor) : RedditPostRepository {
-
-    override fun postsOfSubreddit(subReddit: String, pageSize: Int): Listing<RedditPost> {
-        val sourceFactory = SubRedditDataSourceFactory(redditService, subReddit, networkExecutor)
-
-        val pagedListConfig = PagedList.Config.Builder()
-                .setEnablePlaceholders(false)
-                .setInitialLoadSizeHint(pageSize * 2)
-                .setPageSize(pageSize)
-                .build()
-        val pagedList = LivePagedListBuilder(sourceFactory, pagedListConfig)
-                .setFetchExecutor(networkExecutor)
-                .build()
-
-        val statusUpdates = Transformations.switchMap(sourceFactory.subject, { it.pageSubject })
-
-        return Listing(
-                pagedList = pagedList,
-                statusUpdates = statusUpdates,
-                retry = { sourceFactory.subject.value?.retryFailed() },
-                refresh = { sourceFactory.subject.value?.invalidate() }
-        )
-    }
-}
-*/
 
 class SubRedditDataSourceFactory(
-        private val redditApi: RedditService,
-        private val subredditName: String,
-        private val retryExecutor: Executor
-) : DataSource.Factory<String, RedditPost>() {
-
-    val subject = MutableLiveData<ItemKeyedSubredditDataSource>()
-
-    override fun create(): DataSource<String, RedditPost> {
-        val source = ItemKeyedSubredditDataSource(redditApi, subredditName, retryExecutor)
-        subject.postValue(source)
-        return source
-    }
-}
-
-/*class SubRedditDataSourceFactory(
         private val redditApi: RedditService,
         private val subredditName: String,
         private val retryExecutor: Executor
@@ -166,132 +98,8 @@ class SubRedditDataSourceFactory(
         subject.onNext(source)
         return source
     }
-}*/
+}
 
-/*class ItemKeyedSubredditDataSource(
-        private val redditService: RedditService,
-        private val subredditName: String,
-        private val retryExecutor: Executor
-) : ItemKeyedDataSource<String, RedditPost>() {
-
-    private var retry: (() -> Any)? = null
-
-    val pageSubject: PublishSubject<Result> = PublishSubject.create<Result>()
-
-    fun retryFailed() {
-        val prevRetry = retry
-        retry = null
-        prevRetry?.let { retryExecutor.execute { it.invoke() } }
-    }
-
-    override fun loadInitial(params: LoadInitialParams<String>, callback: LoadInitialCallback<RedditPost>) {
-        redditService
-                .getTop(subredditName, params.requestedLoadSize)
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .doOnSubscribe { pageSubject.onNext(Result(NetworkState.LOADING)) }
-                .subscribe({
-                    val items = it.data.children.map { it.data }
-                    retry = null
-                    pageSubject.onNext(Result(NetworkState.LOADED))
-                    callback.onResult(items)
-                }) {
-                    retry = { loadInitial(params, callback) }
-                    pageSubject.onNext(Result(NetworkState.ERROR))
-                }
-    }
-
-    override fun loadAfter(params: LoadParams<String>, callback: LoadCallback<RedditPost>) {
-        redditService
-                .getTopAfter(subredditName, params.key, params.requestedLoadSize)
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .doOnSubscribe { pageSubject.onNext(Result(NetworkState.LOADING)) }
-                .subscribe({
-                    val items = it.data.children.map { it.data }
-                    retry = null
-                    callback.onResult(items)
-                    pageSubject.onNext(Result(NetworkState.LOADED))
-                }) {
-                    retry = { loadAfter(params, callback) }
-                    pageSubject.onNext(Result(NetworkState.ERROR))
-                }
-    }
-
-    override fun loadBefore(params: LoadParams<String>, callback: LoadCallback<RedditPost>) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun getKey(item: RedditPost): String = item.name
-}*/
-
-/*class ItemKeyedSubredditDataSource(
-        private val redditService: RedditService,
-        private val subredditName: String,
-        private val retryExecutor: Executor
-) : ItemKeyedDataSource<String, RedditPost>() {
-
-    private var retry: (() -> Any)? = null
-
-    val pageSubject: PublishSubject<Result> = PublishSubject.create<Result>()
-
-    fun retryFailed() {
-        val prevRetry = retry
-        retry = null
-        prevRetry?.let { retryExecutor.execute { it.invoke() } }
-    }
-
-    override fun loadInitial(params: LoadInitialParams<String>, callback: LoadInitialCallback<RedditPost>) {
-        val request = redditService.getTop(subredditName, params.requestedLoadSize)
-
-        try {
-            val response = request.execute()
-            val items = response.body()?.data?.children?.map { it.data }
-            retry = null
-            pageSubject.onNext(Result(NetworkState.LOADED))
-            callback.onResult(items!!)
-        } catch (e: IOException) {
-            retry = { loadInitial(params, callback) }
-            pageSubject.onNext(Result(NetworkState.ERROR))
-        }
-
-    }
-
-    override fun loadAfter(params: LoadParams<String>, callback: LoadCallback<RedditPost>) {
-        redditService
-                .getTopAfter(subredditName, params.key, params.requestedLoadSize)
-                .enqueue(object : retrofit2.Callback<RedditService.ListingResponse> {
-                    override fun onFailure(
-                            call: Call<RedditService.ListingResponse>?,
-                            t: Throwable?) {
-                        retry = { loadAfter(params, callback) }
-                        pageSubject.onNext(Result(NetworkState.ERROR))
-                    }
-
-                    override fun onResponse(
-                            call: Call<RedditService.ListingResponse>?,
-                            response: Response<RedditService.ListingResponse>) {
-                        if (response.isSuccessful) {
-                            val items = response.body()?.data?.children?.map { it.data }
-                            retry = null
-                            callback.onResult(items!!)
-                            pageSubject.onNext(Result(NetworkState.LOADED))
-                        } else {
-                            retry = { loadAfter(params, callback) }
-                            pageSubject.onNext(Result(NetworkState.ERROR))
-                        }
-                    }
-                })
-
-
-    }
-
-    override fun loadBefore(params: LoadParams<String>, callback: LoadCallback<RedditPost>) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun getKey(item: RedditPost): String = item.name
-}*/
 
 class ItemKeyedSubredditDataSource(
         private val redditService: RedditService,
@@ -301,7 +109,7 @@ class ItemKeyedSubredditDataSource(
 
     private var retry: (() -> Any)? = null
 
-    val pageSubject = MutableLiveData<Result>()
+    val pageSubject = PublishSubject.create<Result>()
 
     fun retryFailed() {
         val prevRetry = retry
@@ -316,11 +124,11 @@ class ItemKeyedSubredditDataSource(
             val response = request.execute()
             val items = response.body()?.data?.children?.map { it.data }
             retry = null
-            pageSubject.postValue(Result(NetworkState.LOADED))
+            pageSubject.onNext(Result.NetworkStateResult(NetworkState.LOADED))
             callback.onResult(items!!)
         } catch (e: IOException) {
             retry = { loadInitial(params, callback) }
-            pageSubject.postValue(Result(NetworkState.ERROR))
+            pageSubject.onNext(Result.NetworkStateResult(NetworkState.ERROR))
         }
 
     }
@@ -333,7 +141,7 @@ class ItemKeyedSubredditDataSource(
                             call: Call<RedditService.ListingResponse>?,
                             t: Throwable?) {
                         retry = { loadAfter(params, callback) }
-                        pageSubject.postValue(Result(NetworkState.ERROR))
+                        pageSubject.onNext(Result.NetworkStateResult(NetworkState.ERROR))
                     }
 
                     override fun onResponse(
@@ -343,10 +151,10 @@ class ItemKeyedSubredditDataSource(
                             val items = response.body()?.data?.children?.map { it.data }
                             retry = null
                             callback.onResult(items!!)
-                            pageSubject.postValue(Result(NetworkState.LOADED))
+                            pageSubject.onNext(Result.NetworkStateResult(NetworkState.LOADED))
                         } else {
                             retry = { loadAfter(params, callback) }
-                            pageSubject.postValue(Result(NetworkState.ERROR))
+                            pageSubject.onNext(Result.NetworkStateResult(NetworkState.ERROR))
                         }
                     }
                 })
@@ -360,7 +168,10 @@ class ItemKeyedSubredditDataSource(
     override fun getKey(item: RedditPost): String = item.name
 }
 
-class Result(val networkState: NetworkState)
+sealed class Result {
+    class PagedListResult(val list: PagedList<RedditPost>) : Result()
+    class NetworkStateResult(val networkState: NetworkState) : Result()
+}
 
 sealed class UiIntent {
     class ShowReddit(val subredditName: String) : UiIntent()
